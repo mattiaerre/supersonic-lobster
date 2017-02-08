42c404bfcdf3be1b4605ab75a5c27a8beddadea6
mattia richetto <mrichetto@opentable.com>
2017-02-07 22:39:39 -0800

    consume falcor endpoint w/ react

--- public/index.js
+++
@@ -1,9 +0,0 @@
-((falcor) => {
-  const model = new falcor.Model({ source: new falcor.HttpDataSource('/model.json') });
-  model
-    .get('greeting')
-    .then((response) => {
-      document.getElementById('greeting').innerHTML = response.json.greeting;
-    });
-})(window.falcor);
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import model from './model';
 
-render(
-  <div>Hello from React</div>,
-  document.getElementById('app')
-);
+model.get('greeting')
+  .then((response) => {
+    render(
+      <div>
+        <div>Hello from React</div>
+        <div>{response.json.greeting}</div>
+      </div>,
+      document.getElementById('app')
+    );
+  });
