667e87ceafda615a4510ad690d094c3fe4cb84a0
mattia richetto <mrichetto@opentable.com>
2017-03-16 21:22:18 -0700

    unfalcorify

--- falcor-routes/index.js
+++
@@ -1,8 +0,0 @@
-const apod = require('apod-graphistry-falcor-routes');
-
-const falcorRoutes = () => { // eslint-disable-line arrow-body-style
-  return []
-    .concat(apod(process.env.NASA_API_KEY));
-};
-
-module.exports = falcorRoutes;
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
 
+const debug = require('debug')('supersonic-lobster:index');
 const express = require('express');
 const path = require('path');
-const falcorExpress = require('falcor-express');
-const Router = require('@graphistry/falcor-router');
-const debug = require('debug')('supersonic-lobster:index');
-const falcorPostman = require('falcor-postman');
 const index = require('./routes/index');
-const falcorRoutes = require('./falcor-routes');
 const ocSsrWrapper = require('./middlewares/oc-ssr');
 
 const app = express();
@@ -24,13 +20,6 @@ app.use(express.static(path.join(__dirname, '/node_modules')));
 app.use(ocSsrWrapper({ serverRendering: process.env.OC_REGISTRY_BASE_URL }));
 
 app.use('/', index);
-
-const options = { middlewarePath: '/falcor-postman', falcorModelPath: '/api/v1/model.json', app };
-app.use(falcorPostman(options));
-
-app.use('/api/v1/model.json', falcorExpress.dataSourceRoute((req, res) => { // eslint-disable-line no-unused-vars, arrow-body-style
-  return new Router(falcorRoutes());
-}));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
